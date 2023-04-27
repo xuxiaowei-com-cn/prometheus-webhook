415363c36b2b544c7e279f8614467233c445586d
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-04-27 20:00:56 +0800

    :sparkles: 端口：参数、环境变量

--- prometheus_webhook.go
+++ prometheus_webhook.go
@@ -19,30 +19,47 @@ var (
 
 func main() {
 
+	// 信息
+	info()
+
+	var version bool
+	var port string
+	flag.BoolVar(&version, "version", false, "查看版本号")
+	flag.StringVar(&port, "port", "", "参数中的端口")
+
+	flag.Parse()
+
+	if version {
+		return
+	}
+
+	http.HandleFunc("/", indexHandler)
+
+	if port == "" {
+		port = os.Getenv("PORT")
+		if port == "" {
+			port = "8080"
+			log.Printf("默认端口 %s", port)
+		} else {
+			log.Printf("环境中的端口 %s\n", port)
+		}
+	} else {
+		log.Printf("参数中的端口 %s\n", port)
+	}
+
+	log.Printf("侦听端口 %s", port)
+	log.Printf("http://localhost:%s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
+
+// 信息
+func info() {
 	fmt.Printf("Name：%s\n", Name)
 	fmt.Printf("Version：%s\n", Version)
 	fmt.Printf("Author：%s\n", Author)
 	fmt.Printf("Email：%s\n", Email)
 	fmt.Printf("Copyright：%s\n", Copyright)
 	fmt.Printf("Description：%s\n", Description)
-
-	var version bool
-	flag.BoolVar(&version, "version", false, "查看版本号")
-	flag.Parse()
-	if version {
-		return
-	}
-
-	http.HandleFunc("/", indexHandler)
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("默认端口 %s", port)
-	}
-
-	log.Printf("侦听端口 %s", port)
-	log.Printf("http://localhost:%s", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
